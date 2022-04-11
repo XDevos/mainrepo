8ffa3c8805dae1b4d77a72423d69b2188737b225
Xavier Devos <xavierdevos@outlook.fr>
2022-04-11 11:12:01 +0200

    Initial files

---
+++ mainrepo.py
@@ -0,0 +1,10 @@
+import subrepo as sr
+
+def print_mainrepo():
+    print("Thank's to use mainrepo!")
+    return
+
+if __name__ == "__main__":
+    print_mainrepo()
+    print("sub version:" + sr.__version__)
+    sr.print_subrepo()
