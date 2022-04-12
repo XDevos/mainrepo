76d7ad0c3b7aec7a1ff339507b63c1607b59e03d
XDevos <92782954+XDevos@users.noreply.github.com>
2022-04-12 09:40:07 +0200

    Release/0.2.0 (#3)
    * add submodule subrepo
    
    * fix bug with print_subrepo() call
    
    * Feature/after (#2)
    
    * Change branch inside submodule
    
    * add one space after colon
    
    * change sub-branch to dev
    
    * Delete submodule dependancy

--- mainrepo.py
+++ mainrepo.py
@@ -1,4 +1,5 @@
 import subrepo as sr
+from subrepo.subrepo import print_subrepo
 
 def print_mainrepo():
     print("Thank's to use mainrepo!")
@@ -6,5 +7,5 @@ def print_mainrepo():
 
 if __name__ == "__main__":
     print_mainrepo()
-    print("sub version:" + sr.__version__)
-    sr.print_subrepo()
+    print("sub version: " + sr.__version__)
+    print_subrepo()
